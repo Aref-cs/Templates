336ce1182dc40445cced73b25b787d66f357ee6a
Aref-cs <aref.cs@gmail.com>
2019-11-28 23:57:40 +1100

    Panda lib for ontologies
    General Python to Freesurfer link (via Pandas)

---
+++ general_panadas.py
@@ -0,0 +1,23 @@
+
+
+import pandas as pd
+
+df = pd.read_excel(r'freesurfer data file')
+
+#Reading the data based on the Freesurfer ID
+print(df[df.SegId == idFromOntology].'required data cell'.values[0])
+print(df[df.SegId == idFromOntology].'required data cell'.values[0])
+ .
+ .
+ .
+
+#Reading secondary data in case of tracking change or comparison
+norm_a = df[df.SegId == idFromOntology1].'required data cell'.values[0]
+norm_b = df[df.SegId == idFromOntology2].'required data cell'.values[0]
+ .
+ .
+ .
+
+Calculate calculations 
+print("Result of calculations: ", calculations)
+
